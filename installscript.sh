47a405f2465fb6f1b7c6f10c4ee10720e1ea320b
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-08-15 18:39:37 -0400

    Create installscript.sh

---
+++ installscript.sh
@@ -0,0 +1,26 @@
+sudo apt-get install ksh
+sudo apt-get install bash
+
+sudo apt-get install wget
+sudo apt-get install curl
+
+sudo apt-get install git
+
+sudo apt-get install htop
+
+sudo apt-get install tree
+
+sudo apt-get install gcc
+sudo apt-get install g++
+
+sudo apt-get install default-jdk
+
+sudo apt-get install docker
+
+sudo apt-get install mlocate
+
+sudo apt-get install ncdu
+
+sudo apt-get install nasm
+
+sudo apt-get install neovim
